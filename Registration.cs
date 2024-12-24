ed674d1d7ac74ddee239f798a650da085aff10c0
idanka2010 <idan.frolov@gmail.com>
2024-12-24 16:32:29 +0200

    Add project files.

---
+++ Registration.cs
@@ -0,0 +1,21 @@
+﻿namespace Christmas_project
+{
+    public class Registration
+    {
+        public string username;
+        public string email;
+        public string password;
+        public string GetUsername()
+        {
+            return this.username;
+        }
+        public string GetEmail()
+        {
+            return this.email;
+        }
+        public string GetPassword()
+        {
+            return this.password;
+        }
+    }
+}
